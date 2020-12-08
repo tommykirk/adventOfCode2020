52b6c3aa7a66ac3932d21a53a10b4f5c6a6ddc39
Thomas Kirk <kirk.thomas.d@gmail.com>
2020-12-08 11:58:05 -0500

    messy day 8 part 2

--- tommy/day8.py
+++ tommy/day8.py
@@ -1,19 +1,27 @@
 
 
-def part1():
+def part1(changedIndex):
+	global instructions
+	global visitedIndices
 	visitedIndices = set()
 	with open('day8.txt', 'r') as file:
-		instructions = [line for line in file]
+		instructions = [(line.split()[0], line.split()[1]) for line in file]
 		currIndex = 0
 		acc = 0
 		while currIndex not in visitedIndices and currIndex < len(instructions):
-			operation, argument = instructions[currIndex].split()
+			operation, argument = instructions[currIndex][0], instructions[currIndex][1]
+
+			if currIndex == changedIndex:
+				if operation == 'nop':
+					operation = 'jmp'
+				elif operation == 'jmp':
+					operation = 'nop'
 
 			visitedIndices.add(currIndex)
 			if operation == 'acc':
 				acc += int(argument)
 				currIndex += 1
-			elif operation == 'jmp':
+			elif operation == 'jmp' :
 				currIndex += int(argument)
 			elif operation == 'nop':
 				currIndex += 1
@@ -21,9 +29,16 @@ def part1():
 				print('ERROR: invalid operation')
 				break
 
-		print(currIndex)
-		print(acc)
+		# if currIndex == len(instructions):
+		# 	print(changedIndex)
+		return currIndex == len(instructions), acc
 
+part1(-1)
 
+for index in visitedIndices:
+	if instructions[index][0] == 'acc':
+		continue
+	else:
+		if part1(index)[0]:
+			print(part1(index)[1])
 
-part1()
