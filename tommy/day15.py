7df577155df7699175236a3727f78e745dc5092e
Thomas Kirk <kirk.thomas.d@gmail.com>
2020-12-15 16:36:47 -0500

    simplify day 15

--- tommy/day15.py
+++ tommy/day15.py
@@ -6,33 +6,20 @@ inputList = [int(n) for n in '9,12,1,4,17,0,18'.split(',')]
 numberToMostRecentIndexMap = {}
 
 
-for i, n in enumerate(inputList):
-	numberToMostRecentIndexMap[n] = [i]
+for i, n in enumerate(inputList[:-1]):
+	numberToMostRecentIndexMap[n] = i
 
 prevNumSpoken = inputList[-1]
+currNumSpoken = -1
 
 for i in range(len(inputList), 30000000):
-	if prevNumSpoken in numberToMostRecentIndexMap.keys() and len(numberToMostRecentIndexMap[prevNumSpoken]) == 2:
-		prevNumSpoken = numberToMostRecentIndexMap[prevNumSpoken][1] - numberToMostRecentIndexMap[prevNumSpoken][0]
-		if prevNumSpoken in numberToMostRecentIndexMap.keys():
-			if len(numberToMostRecentIndexMap[prevNumSpoken]) == 2:
-				numberToMostRecentIndexMap[prevNumSpoken][0] = numberToMostRecentIndexMap[prevNumSpoken][1]
-				numberToMostRecentIndexMap[prevNumSpoken][1] = i
-			else:
-				numberToMostRecentIndexMap[prevNumSpoken] += [i]
-		else:
-			numberToMostRecentIndexMap[prevNumSpoken] = [i]
+	if prevNumSpoken in numberToMostRecentIndexMap.keys():
+		currNumSpoken = i - 1 - numberToMostRecentIndexMap[prevNumSpoken]
+		numberToMostRecentIndexMap[prevNumSpoken] = i - 1
+		prevNumSpoken = currNumSpoken
 	else:
-		if prevNumSpoken in numberToMostRecentIndexMap.keys() and len(numberToMostRecentIndexMap[prevNumSpoken]) == 1:
-			prevNumSpoken = 0
-			if prevNumSpoken in numberToMostRecentIndexMap.keys():
-				if len(numberToMostRecentIndexMap[prevNumSpoken]) == 2:
-					numberToMostRecentIndexMap[prevNumSpoken][0] = numberToMostRecentIndexMap[prevNumSpoken][1]
-					numberToMostRecentIndexMap[prevNumSpoken][1] = i
-				else:
-					numberToMostRecentIndexMap[prevNumSpoken] += [i]
-			else:
-				numberToMostRecentIndexMap[prevNumSpoken] = [i]
+		numberToMostRecentIndexMap[prevNumSpoken] = i - 1
+		prevNumSpoken = 0
 
 	if i % 1000000 == 0:
 		print(i, prevNumSpoken)
