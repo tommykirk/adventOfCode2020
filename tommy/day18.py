996cd5825a99ae637dab14cc5854069c6c0f39da
Thomas Kirk <kirk.thomas.d@gmail.com>
2020-12-18 15:28:37 -0500

    day 18 part 1

---
+++ tommy/day18.py
@@ -0,0 +1,51 @@
+# 5 * (2 * 8) * 6 + 4
+# 5 * (5 + 2 * 7) * 5
+
+
+
+# (6 * (2 * 7 * 3 * 7 * 2) + 5 * 4 + (2 * 9 + 8 + 3) + 6) * (8 + 3 * 9) * ((9 * 2) + 2)
+
+def parse(parseList):
+	if len(parseList) < 3:
+		return
+	elif  '(' in parseList[-3:]:
+		return
+	else:
+		num2 = parseList.pop()
+		op = parseList.pop()
+		if op == '*':
+			parseList[-1] *= num2
+		else:
+			parseList[-1] += num2
+
+
+
+with open('day18.txt', 'r') as file:
+	sumOfLines = 0
+	i = 0
+	for line in file:
+		i += 1
+		parseList = []
+		for c in line.strip().replace(' ',''):
+			print(parseList, c)
+			if c != ')':
+				if c not in ['(','*','+']:
+					parseList.append(int(c))
+					parse(parseList)
+				else:
+					parseList.append(c)
+			else:
+				while len(parseList) >= 3 and '(' not in parseList[-3:]:
+					parse(parseList)
+				parseList[-2] = parseList[-1]
+				parseList.pop()
+				while len(parseList) >= 3 and '(' not in parseList[-3:]:
+					parse(parseList)
+
+		print(i)
+		if len(parseList) > 1:
+			raise
+		else:
+			sumOfLines += parseList[0]
+
+		print(sumOfLines)
